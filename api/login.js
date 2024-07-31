0db8b7c3b71adcbe4edd6a79253f5d0580e7960e
Ssri2504 <154655702+Ssri2504@users.noreply.github.com>
2024-07-31 18:50:04 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,13 +1,31 @@
-// api/login.js
+const backendUrl = 'https://smard-study.vercel.app/api';
 
-module.exports = (req, res) => {
-    if (req.method === 'POST') {
-      const { username, password } = req.body;
-  
-      // Implement your login logic here
-  
-      res.status(200).json({ message: 'Login successful' });
-    } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
-    }
-  };
+function login() {
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (username === '' || password === '') {
+    alert('Please enter both username and password.');
+    return;
+  }
+
+  fetch(`${backendUrl}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        localStorage.setItem('loggedInUser', JSON.stringify(data.user));
+        alert('Login successful!');
+        navigateTo('profile');
+        displayProfile();
+      }
+    })
+    .catch((error) => console.error('Error:', error));
+}
